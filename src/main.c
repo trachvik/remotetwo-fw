bfe7289be8719e8125feaf3d5706a0204a47d9f8
trachvik <vitrachta@gmail.com>
2026-03-16 15:08:44 +0100

    Logging

--- src/main.c
+++ src/main.c
@@ -1,22 +1,29 @@
 #include <zephyr/kernel.h>
-#include "haptic.h"
-#include "drivers/as5048a.h"
+#include <zephyr/logging/log.h>
+//#include "haptic.h"
+//#include "drivers/as5048a.h"
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 /* Device instances */
-static struct as5048a_device encoder;
-static bldc_driver_6pwm_t driver;
-static bldc_motor_t motor;
+//static struct as5048a_device encoder;
+//static bldc_driver_6pwm_t driver;
+//static bldc_motor_t motor;
 
 /* Global pointer for sensor wrapper */
-struct as5048a_device *g_as5048a = &encoder;
+//struct as5048a_device *g_as5048a = &encoder;
 
 int main(void)
 {
-	/* Initialize haptic motor */
-	haptic_init(&motor, (bldc_driver_t*)&driver, (sensor_t*)&encoder);
+	LOG_INF("System started");
 
-	/* haptic_loop is now driven by a 10 kHz timer thread in haptic.c */
+	/* Initialize haptic motor */
+	//haptic_init(&motor, (bldc_driver_t*)&driver, (sensor_t*)&encoder);
+
+	int counter = 0;
+
 	while (1) {
-		k_sleep(K_FOREVER);
+		LOG_INF("Loop iteration: %d", counter++);
+		k_sleep(K_MSEC(1000));
 	}
 }
