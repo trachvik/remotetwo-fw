d0ee545712c72341a263a6bf1e1079c4a2ba3191
trachvik <vitrachta@gmail.com>
2026-04-09 15:35:48 +0200

    Midpoint bump detent

--- src/haptic.h
+++ src/haptic.h
@@ -10,8 +10,10 @@ int haptic_init(bldc_motor_t *motor, bldc_driver_t *driver, sensor_t *encoder);
 void haptic_loop(bldc_motor_t *motor);
 int haptic_update_num_steps_from_button(void);
 int haptic_get_num_steps(void);
+void haptic_set_num_steps(int steps);
 void haptic_set_step_callback(void (*cb)(int dir));
 void haptic_set_virtual_click_callback(void (*cb)(int dir));
+void haptic_set_virtual_long_hold_callback(void (*cb)(int dir));
 
 
 
