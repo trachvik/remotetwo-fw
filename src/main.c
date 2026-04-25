8e195a8b7d48d076a1cd521e4943d91bb25997ae
trachvik <vitrachta@gmail.com>
2026-04-25 19:08:35 +0200

    Custom disp lib + rest of the code

--- src/main.c
+++ src/main.c
@@ -1,16 +1,34 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include "haptic.h"
+#include "remote_control.h"
+#include "drivers/as5048a.h"
 #include "ui_display.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+/* Device instances */
+static struct as5048a_device encoder;
+static bldc_driver_6pwm_t driver;
+static bldc_motor_t motor;
+
+/* Global pointer for sensor wrapper */
+struct as5048a_device *g_as5048a = &encoder;
 
 int main(void)
 {
 	LOG_INF("System started");
 
+	/* Display before BLE/haptic */
 	if (ui_display_init() == 0) {
 		(void)ui_display_show_hello_remote();
 	}
+
+	/* Initialize BLE */
+	remote_control_init();
+
+	/* Initialize haptic motor */
+	haptic_init(&motor, (bldc_driver_t *)&driver, (sensor_t *)&encoder);
 
 	while (1) {
 		ui_display_process();
